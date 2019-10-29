1170cd87db6f4e04cd4c86830526761cfa77577d
supriyaazure <57016459+supriyaazure@users.noreply.github.com>
2019-10-29 11:52:30 +0530

    test to deploy kudu

--- includes/config.php
+++ includes/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website(Web App)',
+        'name' => 'Simple PHP Website(Web App Kudu)',
         'site_url' => '',
         'pretty_uri' => true,
         'nav_menu' => [
